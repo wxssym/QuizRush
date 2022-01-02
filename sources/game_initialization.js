847f3a5c55ee4bc8eec8a5cd6d946c03a1d55817
wxssym <wassim.sisaid@gmail.com>
2022-01-02 15:48:21 +0100

    main

--- sources/game_initialization.js
+++ sources/game_initialization.js
@@ -1,15 +1,10 @@
 //game initialization
 function initGame() {
   total_players = players_names_list.length;
-    if (total_players > 1) {
-      current_case_num = 0;
-      plateau_setup(settings.number_of_cases, 3);
-      ui_chose_difficulty ();
-    } 
-    else if (total_players==0) {
+    if (total_players==0) {
       alert('There is no players added yet');
     } 
-    else if (total_players==1){
+    else if (total_players>0){
       player_index = 0;
       current_case_num = 0;
       plateau_setup(settings.number_of_cases, 3);
