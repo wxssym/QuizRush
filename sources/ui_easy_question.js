ff54ded6b8272106c05a474ca4660c69d4f6b45e
wxssym <wassim.sisaid@gmail.com>
2022-01-04 17:58:30 +0100

    main

--- sources/ui_easy_question.js
+++ sources/ui_easy_question.js
@@ -6,9 +6,9 @@ async function ui_easy_question() {
     text_Question = createP('...').style('text-align', CENTER).style('transform', 'translate(-50%,-50%)').position(windowWidth / 2, windowHeight / 2 -50).style('color', '#ffffff');
     text_Question.html(await case_question["question"]).style('text-align', CENTER).style('transform', 'translate(-50%,-50%)').position(windowWidth / 2, windowHeight / 2 -50).style('color', '#ffffff');
     button_E_answer = [];
-    button_E_answer[0] = createButton('True').style('transform', 'translate(-100%)').style('width','150px').style('height','50px').position(windowWidth / 2, windowHeight / 2);
+    button_E_answer[0] = createButton('True').style('transform', 'translate(-100%,+50%)').style('width','150px').style('height','50px').position(windowWidth / 2, windowHeight / 2);
     button_E_answer[0].mouseClicked(easy_answer0_selected);
-    button_E_answer[1] = createButton('False').style('transform', 'translate(0%)').style('width','150px').style('height','50px').position(windowWidth / 2, windowHeight / 2);
+    button_E_answer[1] = createButton('False').style('transform', 'translate(0%,+50%)').style('width','150px').style('height','50px').position(windowWidth / 2, windowHeight / 2);
     button_E_answer[1].mouseClicked(easy_answer1_selected);
 }
 
