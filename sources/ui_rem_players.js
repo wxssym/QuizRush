4e73c7b23477b2fbb0fccf2dcaeca64449b39135
wxssym <wassim.sisaid@gmail.com>
2022-01-05 21:02:58 +0100

    main

--- sources/ui_rem_players.js
+++ sources/ui_rem_players.js
@@ -2,7 +2,7 @@ function ui_remove_player(){
     removeElements();
     selection_removePlayer = createSelect().style('transform', 'translate(-50%,-50%)').class('buttons_style');
     selection_removePlayer.position(windowWidth / 2, windowHeight / 2);
-    selection_removePlayer.option('select a player to remove').class('buttons_style');
+    selection_removePlayer.option('select a player to remove');
     for (let ii = 0; ii<players.length;ii++){
         selection_removePlayer.option(players[ii].player_name);
     };
